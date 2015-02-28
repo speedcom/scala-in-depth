71666fd18f1b43a782517e4ee52543b616a60d9f
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-02-28 15:57:39 +0100

    path-dependent type example

--- type-system/types.scala
+++ type-system/types.scala
@@ -25,4 +25,22 @@ x.foo(x.y) // success
 x.foo(y.y) // error - different instances
 
 // !!!
-x.bar(y.y) // success with #
+x.bar(y.y) // success with
+
+// 1. Example using path-dependent types
+case class Board(length: Int, height: Int) {
+  case class Coordinate(x: Int, y: Int) {
+    require(0 <= x && x < length && 0 <= y && y < height)
+  }
+  val occupied = scala.collection.mutable.Set[Coordinate]()
+}
+
+val b1 = Board(10,20)
+val b2 = Board(55, 100)
+val c1 = b1.Coordinate(9,19)
+val c2 = b2.Coordinate(54, 99)
+
+b1.occupied += c1
+b2.occupied += c2
+// this wont compile
+// b2.occupied += c1
