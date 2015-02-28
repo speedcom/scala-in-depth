3b73401420b6bec6f5e3d6d6b2b0d17b3c203620
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-02-28 14:19:13 +0100

    path-dependent types && type-projection

--- type-system/types.scala
+++ type-system/types.scala
@@ -8,3 +8,21 @@ object Simulate {
   }
 }
 Simulate once { () => println("Simulation...")} right Now
+
+// path-dependent types && type-projection
+class Outer {
+  trait Inner
+
+  def y = new Inner {}
+  def foo(x: this.Inner) = null  // using path-dependent type
+  def bar(x: Outer#Inner) = null // using projection of type
+}
+val x = new Outer
+val y = new Outer
+
+x.y // type is shown as x.Inner, not Outer#Inner or Outer.Inner
+x.foo(x.y) // success
+x.foo(y.y) // error - different instances
+
+// !!!
+x.bar(y.y) // success with #
