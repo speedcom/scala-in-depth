3570a039072d02420ea20c9b02400af734b6715e
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-01 18:02:07 +0100

    typo

--- type-system/reification.scala
+++ type-system/reification.scala
@@ -1,7 +1,7 @@
 def peek[A, C <: Traversable[A]](col: C) = (col.head, col)
 
 // compile-error:
-// peak(List(1,2,3)) - engine can not infered A type
+// peek(List(1,2,3)) - engine can not infered A type
 peek[Int, List[Int]](List(1,2,3))
 
 // second version with two list parameter
