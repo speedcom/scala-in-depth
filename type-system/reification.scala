16986b49ef644dd645f840540663c56a3d9af812
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-01 17:47:49 +0100

    second example with reification using

--- type-system/reification.scala
+++ type-system/reification.scala
@@ -1,6 +1,10 @@
-def peak[A, C <: Traversable[A]](col: C) = (col.head, col)
+def peek[A, C <: Traversable[A]](col: C) = (col.head, col)
 
 // compile-error:
 // peak(List(1,2,3)) - engine can not infered A type
-peak[Int, List[Int]](List(1,2,3))
+peek[Int, List[Int]](List(1,2,3))
 
+// second version with two list parameter
+def peek[C, A](col: C)(implicit ev: C <:< Traversable[A]) = (col.head, col)
+
+peek(List(1,2,3))
